dc3d770dab874a81c97c8afd16e91589d464b62f
RBXBlast <52540531+RBXBlast@users.noreply.github.com>
2020-07-10 11:00:22 -0600

    Update main.js

--- Assignement 1/main.js
+++ Assignement 1/main.js
@@ -4,32 +4,26 @@
 let cnv = document.getElementById("myCanvas");
 let ctx = cnv.getContext("2d");
 cnv.width = 1200;
-cnv.height = 1200
-//Shape 1
-rect(5, 5, 290, 150, "black", "7");
-circle(75,75,50,0,2,"black");
-rect(5, 5, 150, 290, "black", "7");
-//Shape 2
-rect(5,225,290,150,"blue","7.5");
-rect(145,225,155,290,"blue","7.5");
-circle(225,300,50,0,2,"blue");
-//Shape 3
-rect(5, 370, 290, 150, "red", "7");
-circle(75,445,65,0,2,"red");
-rect(5, 370, 150, 290, "red", "7");
+cnv.height = 1200;
 
-//Functions
-function rect(x, y, width, height, color, linewidth) {
+drawFace(50, 50, 50, 50, "blue");
+drawFace(175, 175, 75, 175, "red");
+drawFace(700, 200, 150, 50, "green");
+
+function drawFace(x, y, x2, y2, color) {
   ctx.beginPath();
-  ctx.lineWidth = linewidth;
+  ctx.arc(x + 50, y + 50, x2 + 30, y2 + 30, 100 * Math.PI);
   ctx.strokeStyle = color;
-  ctx.rect(x, y, width, height);
   ctx.stroke();
-}
-
-function circle(x1,y1,x2,y2,r1,color) {
   ctx.beginPath();
-  ctx.arc(x1, y1, x2, y2, r1 * Math.PI);
+  ctx.arc(x + 50, y + 50, x2, y2, Math.PI);
+  ctx.strokeStyle = color;
+  ctx.stroke();
+  ctx.beginPath();
+  ctx.arc(x + 30, y + 10, 5, 0, Math.PI * 2);
+  ctx.strokeStyle = color;
+  ctx.stroke();
+  ctx.arc(x + 75, y + 10, 5, 0, Math.PI * 2);
   ctx.strokeStyle = color;
   ctx.stroke();
 }
